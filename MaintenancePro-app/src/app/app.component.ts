4f5d10deacb9bf52c862c1c66d19831ab673f11b
KovacsIdaJusztina <ida.kovacs@gmail.com>
2024-10-05 19:38:03 +0200

    Add Navbar Component

--- MaintenancePro-app/src/app/app.component.ts
+++ MaintenancePro-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TestComponent],
+  imports: [RouterOutlet, TestComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
