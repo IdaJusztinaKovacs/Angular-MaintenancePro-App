40f56448618be58c7f41c6a97307c0e325470aa0
KovacsIdaJusztina <ida.kovacs@gmail.com>
2024-10-05 21:31:58 +0200

    fixed the Navbar Component

--- MaintenancePro-app/src/app/components/test/test.component.ts
+++ MaintenancePro-app/src/app/components/test/test.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
     selector: 'app-test',
     standalone: true,
+    imports: [NavbarComponent],
     template: `
+    <app-navbar></app-navbar>
     <h1>Üdvözöllek az oldalon!</h1>
     <p>Ez egy manuálisan létrehozott teszt komponens</p>`,
     styles: ['h1 {color:red}']
--- MaintenancePro-app/src/main.ts
+++ MaintenancePro-app/src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { TestComponent } from './app/components/test/test.component';
+;
 
 bootstrapApplication(TestComponent, appConfig)
   .catch((err) => console.error(err));
